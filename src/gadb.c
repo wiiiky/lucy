7f1f6716b946c439d020eaa8df4fd9d84d0f8b6d
wiiiky <wiiiky@yeah.net>
2014-09-13 22:47:01 +0800

    加入.gitignore，第一个界面

--- src/gadb.c
+++ src/gadb.c
@@ -38,8 +38,21 @@
 
 int main(int argc, char **argv)
 {
+    gtk_init(&argc, &argv);
     adb_sysdeps_init();
     adb_trace_init();
-    D("Handling commandline()\n");
-    return adb_commandline(argc - 1, argv + 1);
+/*    return adb_commandline(argc - 1, argv + 1);*/
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 960, 640);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_title(GTK_WINDOW(window), "GADB");
+
+    g_signal_connect(G_OBJECT(window), "destroy",
+                     G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
 }
