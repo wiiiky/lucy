d2dba56d800a9fc91ca404784b9ea28bd931fb22
wiiiky <wiiiky@yeah.net>
2014-09-14 20:17:42 +0800

    menubar

--- src/gadb.c
+++ src/gadb.c
@@ -35,6 +35,55 @@
 #include "adb_auth.h"
 #include "usb_vendors.h"
 
+#define PROGRAM_NAME "GADB"
+#define GADB_WEBSITE "https://github.com/wiiiky/gadb"
+#define GADB_COMMENTS "Android Manager for Linux"
+#define GADB_COPYRIGH "Copyright (c) Wiky L"
+#define GADB_VERSION "0.1"
+const static gchar *GADB_AUTHORS[] = {
+    "Wiky L (wiiiky@yeah.net)",
+    NULL
+};
+
+static void onAboutMenuItemActivate(GtkMenuItem * item, gpointer data);
+
+static GtkWidget *createMenuBar()
+{
+    GtkWidget *menuBar = gtk_menu_bar_new();
+    GtkWidget *aboutItem = gtk_menu_item_new_with_mnemonic("_About");
+    gtk_menu_shell_append(GTK_MENU_SHELL(menuBar), aboutItem);
+
+    GtkWidget *aboutMenu = gtk_menu_new();
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(aboutItem), aboutMenu);
+
+    GtkWidget *_aboutItem = gtk_menu_item_new_with_mnemonic("_About");
+    gtk_menu_shell_append(GTK_MENU_SHELL(aboutMenu), _aboutItem);
+    g_signal_connect(G_OBJECT(_aboutItem), "activate",
+                     G_CALLBACK(onAboutMenuItemActivate), NULL);
+
+    return menuBar;
+}
+
+
+static void onAboutMenuItemActivate(GtkMenuItem * item, gpointer data)
+{
+    GtkWidget *dialog = gtk_about_dialog_new();
+    gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(dialog),
+                                      PROGRAM_NAME);
+    gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dialog), GADB_VERSION);
+    gtk_about_dialog_set_license_type(GTK_ABOUT_DIALOG(dialog),
+                                      GTK_LICENSE_GPL_3_0);
+    gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(dialog),
+                                   GADB_COPYRIGH);
+    gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog), GADB_COMMENTS);
+    gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog), GADB_WEBSITE);
+    gtk_about_dialog_set_authors(GTK_ABOUT_DIALOG(dialog), GADB_AUTHORS);
+
+    gtk_dialog_run(GTK_DIALOG(dialog));
+
+    gtk_widget_destroy(dialog);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -47,9 +96,13 @@ int main(int argc, char **argv)
     gtk_window_set_default_size(GTK_WINDOW(window), 960, 640);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_title(GTK_WINDOW(window), "GADB");
-
     g_signal_connect(G_OBJECT(window), "destroy",
                      G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *rootBox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add(GTK_CONTAINER(window), rootBox);
+
+    gtk_box_pack_start(GTK_BOX(rootBox), createMenuBar(), FALSE, FALSE, 0);
 
     gtk_widget_show_all(window);
     gtk_main();
