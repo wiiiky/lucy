0aacb5e68d524e9cf7648ec7fa8390f24b128678
jiajun <gansteed@gmail.com>
2014-10-25 21:12:19 +0800

    add getfont()

---
+++ src/ui_getfont.h
@@ -0,0 +1,13 @@
+/*
+ * use gsettings to get system default font name and size
+ */
+
+#ifndef UI_GETFONT_H
+#define UI_GETFONT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <glib.h>
+void getfont(gchar *);
+
+#endif
