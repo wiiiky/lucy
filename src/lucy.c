4a2a7dd61dcfb7e44b7d0627954d08f3744aa490
wiiiky <wiiiky@yeah.net>
2014-11-25 12:44:43 +0800

    最近工作比较忙

--- src/lucy.c
+++ src/lucy.c
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#define  TRACE_TAG   TRACE_ADB
+/*#define  TRACE_TAG   TRACE_ADB*/
 
 #include <stdio.h>
 #include <stdlib.h>
